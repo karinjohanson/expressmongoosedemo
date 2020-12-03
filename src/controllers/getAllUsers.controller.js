18e8b54cfa93f7aa85537e5313dd1d6889bca516
Karin Kikas <karin.kikas>
2020-12-03 15:13:58 +0200

    Backend age=true tehtud

--- src/controllers/getAllUsers.controller.js
+++ src/controllers/getAllUsers.controller.js
@@ -3,7 +3,15 @@ const Users = db.Users
 
 module.exports = async function (req, res) {
   try {
-    const allUsers = await Users.find({age: {$exists: true}})
+    const filter = {}
+    filter = req.query
+    if (req.query.age === 'true') {
+      filter.age = {$exists: true}
+    }
+
+    console.log(filter)
+    
+    const allUsers = await Users.find(filter)
     res.status(200).json({ allUsers })
   } catch (error) {
     res.status(500).json({ message: error })
