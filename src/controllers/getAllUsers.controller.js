d6e411341c20c6139c6f8107f59957776c7b43f1
Karin Kikas <karin.kikas>
2020-12-03 15:05:17 +0200

    Age greater than

--- src/controllers/getAllUsers.controller.js
+++ src/controllers/getAllUsers.controller.js
@@ -3,7 +3,7 @@ const Users = db.Users
 
 module.exports = async function (req, res) {
   try {
-    const allUsers = await Users.find({age: {$exists: true}})
+    const allUsers = await Users.find({age: {$gt: 0}})
     res.status(200).json({ allUsers })
   } catch (error) {
     res.status(500).json({ message: error })
